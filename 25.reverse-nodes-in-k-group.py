d21f6ad7882adde831fa6d2303e1589c97f00436
Ricebucket <88389934+r1cebucket@users.noreply.github.com>
2022-07-24 17:22:46 +0000

    test

---
+++ 25.reverse-nodes-in-k-group.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=25 lang=python3
+#
+# [25] Reverse Nodes in k-Group
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        print(head.val)
+# @lc code=end
